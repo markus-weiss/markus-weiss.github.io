d56cdfb16b32a29d044c5323d8a2f616c991bd02
Anna Lotz <37657636+AnnaLotz@users.noreply.github.com>
2022-03-22 10:49:12 +0100

    created basic doc-structure and linking

---
+++ sources/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var MyPortfolio;
+(function (MyPortfolio) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log("Hello World, Anna here");
+    }
+})(MyPortfolio || (MyPortfolio = {}));
+//# sourceMappingURL=main.js.map
---
+++ sources/scripts/main.ts
@@ -0,0 +1,11 @@
+namespace MyPortfolio {
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+
+        console.log("Hello World, Anna here");
+    }
+
+
+}
